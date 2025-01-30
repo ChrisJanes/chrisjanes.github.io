02ad24c7e239b52c97422c45eff0438c2c865de8
Chris Janes <gingoreilly@gmail.com>
2025-01-30 10:34:04 +0000

    add linked_lists talk and update index pages.

---
+++ talks/linked_lists/assets/slidev/default-D_iS-y7Q.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-DfLVPQLi.js";import{_ as r}from"../index-Co7tSS5i.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
