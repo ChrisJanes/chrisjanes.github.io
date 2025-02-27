105c07410848443fdec3a4cc89db587c2a8cbdda
Chris Janes <gingoreilly@gmail.com>
2025-02-27 17:25:36 +0000

    update Trees slides to add columns to TOC
    update index pages to put graph card into columns

---
+++ talks/trees/assets/slidev/default-B0xix2cY.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-d8n_xe2r.js";import{_ as r}from"../index-BIDGse0X.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
