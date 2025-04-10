c897e7e02ac0584460c449a71cf2932f96b74396
Chris Janes <gingoreilly@gmail.com>
2025-04-10 08:56:35 +0100

    add programming paradigms slides

---
+++ talks/paradigms/assets/slidev/default-BAug8Zbq.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-D-0H7V9X.js";import{_ as r}from"../index-BttTfrGi.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
